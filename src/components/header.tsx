27a70e15b769bc562c752e793f9cd6d4eeebd624
Bhaaliver <oliver@warmbowski.org>
2024-02-13 15:35:57 -0800

    added fake data with actions, name, ac, maxhp

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,4 @@
-export function Header(props:{characterName: string, AC: number, hp: number}){
-    return 
+export function Header(props:{characterName: string, AC: number, HP: number}){
+    const {characterName, AC, HP} = props
+    return <><h1>{characterName}</h1><br /><h2>AC {AC} Max HP {HP}</h2></>
 }
