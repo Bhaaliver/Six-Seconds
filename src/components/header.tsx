e8362484dcf452db8d191507f75f08bdb0380792
Bhaaliver <oliver@warmbowski.org>
2024-02-14 09:16:10 -0800

    added a local sorage for current hp

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,7 @@
+import { useLocalStorage } from "usehooks-ts";
+
 export function Header(props:{characterName: string, AC: number, HP: number}){
     const {characterName, AC, HP} = props
-    return <><h1>{characterName}</h1><br /><h2>AC {AC} Max HP {HP}</h2></>
+    const [currentHP, setCurrentHP] = useLocalStorage("currentHP", HP)
+    return <><h1>{characterName}</h1><h2>AC {AC} Max HP {HP} currentHP {currentHP}</h2></>
 }
