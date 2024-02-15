cb21a829b6eb4cc77c264a48db5ecc9be5539f2a
Bhaaliver <oliver@warmbowski.org>
2024-02-15 14:41:26 -0800

    shows multiple actions on screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
 import { Header } from "./components/Header"
+import { Actions } from "./fakeData"
+import { Action } from "./components/Action"
 
 function App() {
 
-  return <Header />
+  return <><Header /><br />{Actions.map((action,index)=><Action key={index} name={action.name} description={action.description}/>)}</>
 }
 
 export default App
