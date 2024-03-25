28d76fb66d8cbcfe6dcb06a2b3f7d8a481d7a799
Oliver Laskowski <112512000+Bhaaliver@users.noreply.github.com>
2024-03-25 10:41:04 -0700

    installed tanstack router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css'
 import { Header } from "./components/Header"
+import {Router, RouterProvider} from "@tanstack/react-router"
 
 function App() {
   return(
-    <Header/>
+    <></>
   )
 }
 
